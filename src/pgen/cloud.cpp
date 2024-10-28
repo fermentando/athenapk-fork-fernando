81c3ef910f463e94554c47f99506694109b50525
Fernando Hidalgo Pineda <ferhi@freya01.bc.rzg.mpg.de>
2024-10-28 16:10:55 +0100

    Tracking of single cloud

--- src/pgen/cloud.cpp
+++ src/pgen/cloud.cpp
@@ -276,4 +276,126 @@ parthenon::AmrTag ProblemCheckRefinementBlock(MeshBlockData<Real> *mbd) {
   return parthenon::AmrTag::same;
 };
 
+
+// Compute frame_boosting velocity
+parthenon::TaskStatus
+compute_frame_v(parthenon::MeshData<parthenon::Real> *md) {
+
+  using parthenon::IndexDomain;
+  using parthenon::IndexRange;
+  using parthenon::Real;
+
+  auto pmb = md->GetBlockData(0)->GetBlockPointer();
+  auto hydro_pkg = pmb->packages.Get("Hydro");
+  const auto &prim_pack = md->PackVariables(std::vector<std::string>{"prim"});
+  const auto &cons_pack = md->PackVariables(std::vector<std::string>{"cons"});
+  IndexRange ib = md->GetBlockData(0)->GetBoundsI(IndexDomain::interior);
+  IndexRange jb = md->GetBlockData(0)->GetBoundsJ(IndexDomain::interior);
+  IndexRange kb = md->GetBlockData(0)->GetBoundsK(IndexDomain::interior);
+  const auto nhydro = hydro_pkg->Param<int>("nhydro");
+  const auto nscalars = hydro_pkg->Param<int>("nscalars");
+
+  const auto units = hydro_pkg->Param<Units>("units");
+  Real mean_molecular_mass_by_kb = hydro_pkg->Param<Real>("singlecloud::mean_molecular_mass_by_kb");
+  Real IM_cold_gas;
+  Real md_cold_gas;
+
+
+  pmb->par_reduce(
+      "SingleCloud::frame_boosting_velocity", 0, cons_pack.GetDim(5) - 1, kb.s, kb.e, jb.s, jb.e, ib.s,
+      ib.e,
+      KOKKOS_LAMBDA(const int &b, const int &k, const int &j, const int &i, 
+      Real &local_IM_cold_gas) {
+        auto &prim = prim_pack(b);
+        auto &cons = cons_pack(b);
+        const auto &coords = cons_pack.GetCoords(b);
+        if ( coords.Xc<1>(i) < 0 ) {
+
+
+          if (temp <= 2*T_cloud) {
+
+            const Real cell_cold_mass = prim(IDN, k, j, i) * coords.CellVolume(k, j, i);
+            const Real cold_v3 = pow(cons(IM3, k, j, i), 2);
+            const Real cold_v2 = pow(cons(IM2, k, j, i), 2);
+            const Real cold_v1 = pow(cons(IM1, k, j, i), 2);
+            const Real cold_gas_speed = std::sqrt(cold_v1 + cold_v2 + cold_v3);
+
+            local_IM_cold_gas += cell_cold_mass * cold_gas_speed;
+
+          }
+        }
+      },
+      Kokkos::Sum<Real>(IM_cold_gas)); //parthenon_output
+
+
+  pmb->par_reduce(
+      "SingleCloud:frame_boosting_velocity", 0, cons_pack.GetDim(5) - 1, kb.s, kb.e, jb.s, jb.e, ib.s,
+      ib.e,
+      KOKKOS_LAMBDA(const int &b, const int &k, const int &j, const int &i,
+       Real &local_cold_gas) {
+        auto &prim = prim_pack(b);
+        auto &cons = cons_pack(b);
+        const auto &coords = cons_pack.GetCoords(b);
+        if ( coords.Xc<1>(i) < 0 ) {
+
+          if (temp <= 2*T_cloud) {
+
+            const Real cell_cold_mass = prim(IDN, k, j, i) * coords.CellVolume(k, j, i);
+            const Real cold_v3 = pow(cons(IM3, k, j, i), 2);
+            const Real cold_v2 = pow(cons(IM2, k, j, i), 2);
+            const Real cold_v1 = pow(cons(IM1, k, j, i), 2);
+            const Real cold_gas_speed = std::sqrt(cold_v1 + cold_v2 + cold_v3);
+
+            local_cold_gas+= cell_cold_mass;
+
+          }
+        }
+      }, Kokkos::Sum<Real>(md_cold_gas));
+
+    Real frame_v = IM_cold_gas / md_cold_gas;
+    if (frame_v != 0.) hydro_pkg->UpdateParam("inertial_frame_v", frame_v); 
+
+    return TaskStatus::complete;
+
+}
+
+
+// Shift velocities to maintain intertial frame
+parthenon::TaskStatus
+apply_frame_boost(parthenon::MeshData<parthenon::Real> *md) {
+
+  using parthenon::IndexDomain;
+  using parthenon::IndexRange;
+  using parthenon::Real;
+
+  auto pmb = md->GetBlockData(0)->GetBlockPointer();
+  auto hydro_pkg = md->GetBlockData(0)->GetBlockPointer()->packages.Get("Hydro");
+  const auto &prim_pack = md->PackVariables(std::vector<std::string>{"prim"});
+  const auto &cons_pack = md->PackVariables(std::vector<std::string>{"cons"});
+  IndexRange ib = md->GetBlockData(0)->GetBoundsI(IndexDomain::interior);
+  IndexRange jb = md->GetBlockData(0)->GetBoundsJ(IndexDomain::interior);
+  IndexRange kb = md->GetBlockData(0)->GetBoundsK(IndexDomain::interior);
+  const auto nhydro = hydro_pkg->Param<int>("nhydro");
+  const auto nscalars = hydro_pkg->Param<int>("nscalars");
+
+  Real frame_v = hydro_pkg->Param<Real>("inertial_frame_v");
+  if (fabs(frame_v) > 100.01 || frame_v < 0.0) frame_v = 0.;
+
+
+  parthenon::par_for(
+      DEFAULT_LOOP_PATTERN, "SingleCloud::AdjustingFrameSpeed", parthenon::DevExecSpace(), 0,
+      cons_pack.GetDim(5) - 1, 0, cons_pack.GetDim(4) - 1, kb.s, kb.e, jb.s, jb.e, ib.s,
+      ib.e + 1, 
+      KOKKOS_LAMBDA(const int &b, const int &k, const int &j, const int &i, const Real& frame_v) {
+        auto &prim = prim_pack(b);
+        auto &cons = cons_pack(b);
+
+        cons(IM2, k, j, i) -= frame_v * prim(IDN, k, j, i);
+        // Should add output to track x_shift?
+      });
+
+    return TaskStatus::complete; //compute only every 100 timesteps
+  // output prior to every output
+}
+
 } // namespace cloud
