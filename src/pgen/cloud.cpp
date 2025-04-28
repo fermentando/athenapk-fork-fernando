48e7d6ed7983a0a3a34735a1c29a07d2d23d629d
fermentando <fernando@mpa-garching.mpg.de>
2025-04-28 18:49:31 +0200

    RH jump conditions single cloud

--- src/pgen/cloud.cpp
+++ src/pgen/cloud.cpp
@@ -54,14 +54,29 @@ void InitUserMeshData(Mesh *mesh, ParameterInput *pin) {
 
   r_cloud = pin->GetReal("problem/cloud", "r0_cgs") / units.code_length_cgs();
   rho_cloud = pin->GetReal("problem/cloud", "rho_cloud_cgs") / units.code_density_cgs();
-  rho_wind = pin->GetReal("problem/cloud", "rho_wind_cgs") / units.code_density_cgs();
-  auto T_wind = pin->GetReal("problem/cloud", "T_wind_cgs");
-  auto v_wind = pin->GetReal("problem/cloud", "v_wind_cgs") /
-                (units.code_length_cgs() / units.code_time_cgs());
-  //auto Mach_wind = pin->GetReal("problem/cloud", "Mach_wind");
+  rho_amb = pin->GetReal("problem/cloud", "rho_wind_cgs") / units.code_density_cgs();
+  auto T_amb = pin->GetReal("problem/cloud", "T_wind_cgs");
 
   // mu_mh_gm1_by_k_B is already in code units
-  rhoe_wind = T_wind * rho_wind / mbar_over_kb / gm1;
+  pressure = T_wind * rho_wind / mbar_over_kb / gm1;
+
+  auto mach = pin->GetReal("problem/cloud", "mach_shock");
+
+  // Uses Rankine Hugoniot relations for adiabatic gas to initialize problem
+  Real jump1 = (gamma + 1.0) / (gm1 + 2.0 / (mach * mach));
+  Real jump2 = (2.0 * gamma * mach * mach - gm1) / (gamma + 1.0);
+  Real jump3 = 2.0 * (1.0 - 1.0 / (mach * mach)) / (gamma + 1.0);
+
+  rho_wind = rho_amb * jump1;
+  const auto pressure_wind = pressure * jump2;
+  rhoe_wind = pressure_wind / gm1;
+  const auto T_wind = pressure_wind / rho_wind * mbar_over_kb;
+
+  const auto v_wind = jump3 * mach * std::sqrt(gamma * pressure / rho_amb);
+
+
+
+
   const auto c_s_wind = std::sqrt(gamma * gm1 * rhoe_wind / rho_wind);
   const auto chi_0 = rho_cloud / rho_wind;               // cloud to wind density ratio
   Mach_wind =  v_wind / c_s_wind;
